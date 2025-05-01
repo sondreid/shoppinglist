bb20675ed9c37d9091f5434f9e74b6d107790478
sondre <sondre.eide@gmail.com>
2025-05-01 12:15:09 +0200

    backend adds shoppingitem

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,17 +1,27 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Models;
 
 
-const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+const string allow_origins = "frontend_ports";
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Handleliste API", Version = "v1" });
+});
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
+    options.AddPolicy(name: allow_origins,
                       policy  =>
                       {
-                          policy.WithOrigins("http://localhost:4000", "http://localhost:3000")
+                          policy.WithOrigins("http://localhost:4000", "http://localhost:4001", "http://frontend:4000","http://localhost:3000", "http://172.18.0.3:4000", "http://172.18.0.2:4000", "http://172.18.0.2:5058")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
@@ -19,9 +29,75 @@ builder.Services.AddCors(options =>
 
 
 var app = builder.Build();
+//app.MapControllers();
 
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(allow_origins);
 
 app.MapGet("/", () => new { payload = "hello world" });
+app.MapPost("/storeitem", async (HttpContext context) =>
+{
+    try 
+    {
+        using var reader = new StreamReader(context.Request.Body);
+        var jsonBody = await reader.ReadToEndAsync();
+        console.WriteLine(jsonBody);
+        string filePath = "items.json";
+        await File.WriteAllTextAsync(filePath, jsonBody);
+        
+        return Results.Json(new { success = true, message = $"Data stored successfully {jsonBody}" });
+    }
+    catch (Exception ex)
+    {
+        return Results.Json(new { success = false, message = ex.Message }, statusCode: 500);
+    }
+});
 
-app.Run("http://localhost:5058");
+
+app.MapDelete("/deleteitem", async (HttpContext context) =>
+{
+    try 
+    {
+        string filePath = "items.json";
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+            return Results.Json(new { success = true, message = "File deleted successfully" });
+        }
+        else
+        {
+            return Results.Json(new { success = false, message = "File not found" }, statusCode: 404);
+        }
+    }
+    catch (Exception ex)
+    {
+        return Results.Json(new { success = false, message = ex.Message }, statusCode: 500);
+    }
+});
+
+
+app.Use(async (context, next) =>    
+{
+    if(context.Request.Path.Value == "/favicon.ico")
+    {
+        // Favicon request, return 404
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        return;
+    }
+    
+    // No favicon, call next middleware
+    await next.Invoke();
+});
+
+app.MapGet("/health", () => new { payload = "A-okay" });
+
+
+if ( Environment.GetEnvironmentVariable("dev") != "true")
+{
+   app.Run("http://0.0.0.0:5058");
+}
+
+else
+{
+    app.Run("http://localhost:5058");
+}
+
